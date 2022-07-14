aaf7253d5434b7cb8ee55a8e803bdbfc38d3b850
zhanghao1999math <103239568+Zhanghao1999math@users.noreply.github.com>
2022-07-14 16:28:24 +0800

    Create gtag.js

---
+++ gtag.js
@@ -0,0 +1,9 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-G87X76KYSX"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-G87X76KYSX');
+</script>

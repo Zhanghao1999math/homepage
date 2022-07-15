cd86cac3065be1bc0ee497cb3873e015d4bc4aa5
zhanghao1999math <103239568+Zhanghao1999math@users.noreply.github.com>
2022-07-15 16:35:15 +0800

    Delete gtag.js

--- gtag.js
+++
@@ -1,9 +0,0 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-G87X76KYSX"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-G87X76KYSX');
-</script>
